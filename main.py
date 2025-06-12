3e35ead9d7ffc34eff1fc0bdaccc3400e1349bd9
Jacques Lavoie <46101717+Stormyisland@users.noreply.github.com>
2025-06-11 18:19:14 -0700

    Update main.py

--- main.py
+++ main.py
@@ -15,4 +15,27 @@ class Blockchain:
             'index': len(self.chain) = 1,
             'timestamp': time(),
             'transaction': self.pending_transactions,
+            ' proof' : proof,
+            ' pervious_hash' : previous_hash or self.hash (self.chain[-1]),
+        }
+        self.pending_transactions = []
+        self.chain.append(block)
+        return block
+
+    def new_transaction(self, sender, recipient, amount, conttract_id=None, function= None, args=None):
+        transaction= {
+            'sender': sender,
+            'recipient': recipient,
+            'amount' : amount,
+            'contract_id' : contract_id,
+            'function': function,
+            'args': args
+        }
+        self.pending_transactions.append(transaction)
+        return self.last_block['index'] = 1
+
+    def deploy contract(self, code, sender):
+        contract_id = str(uuid4())
+    
+            
     
